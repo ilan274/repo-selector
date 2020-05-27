23a4fc34af0df128cd51dab75cba6fe51d48eebb
ilan274 <ilan.herbach@gmail.com>
2020-05-27 09:30:09 -0300

    Updates

---
+++ precache-manifest.3cc664668635db99c823ddc644db388f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf78d8e634160c13c3b7e61c0b35bc89",
+    "url": "/repo-selector/index.html"
+  },
+  {
+    "revision": "8ddca23cbc04ab0156bc",
+    "url": "/repo-selector/static/css/main.0ae9e204.chunk.css"
+  },
+  {
+    "revision": "a1e7bb91c3690efd186b",
+    "url": "/repo-selector/static/js/2.80b00e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repo-selector/static/js/2.80b00e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddca23cbc04ab0156bc",
+    "url": "/repo-selector/static/js/main.1a2d6e9c.chunk.js"
+  },
+  {
+    "revision": "534c885351758e6c4bb4",
+    "url": "/repo-selector/static/js/runtime-main.da9a5e74.js"
+  },
+  {
+    "revision": "441bc1760a74f12022c8d65ae95ac8a7",
+    "url": "/repo-selector/static/media/repo.441bc176.png"
+  }
+]);
